d460ed907754e9cfb56ab9f92a1e1cd855510db5
dvir.monajem <dmenajem@gmail.com>
2024-03-10 14:39:03 +0200

    weather-space - layout

--- apps/weather-space/src/app/components/layout/layout.component.ts
+++ apps/weather-space/src/app/components/layout/layout.component.ts
@@ -2,16 +2,22 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
-import { MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import {
+  MatSidenavContainer,
+  MatSidenavContent,
+} from '@angular/material/sidenav';
 
 @Component({
-    selector: 'weather-space-layout',
-    templateUrl: './layout.component.html',
-    styleUrls: ['./layout.component.scss'],
-    standalone: true,
-    imports: [MatSidenavContainer, MatSidenavContent, MatToolbar, MatButton, RouterLink]
+  selector: 'weather-space-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+  standalone: true,
+  imports: [
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatToolbar,
+    MatButton,
+    RouterLink,
+  ],
 })
-export class LayoutComponent {
-
-
-}
+export class LayoutComponent {}

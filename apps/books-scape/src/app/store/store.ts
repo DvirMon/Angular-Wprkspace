489fe26599af053352987dbc7d8257a94a541cd0
dvir.monajem <dmenajem@gmail.com>
2024-03-14 16:42:27 +0200

    books - update service names

--- apps/books-scape/src/app/store/store.ts
+++ apps/books-scape/src/app/store/store.ts
@@ -1,25 +1,15 @@
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
 import { signalStore, withHooks } from '@ngrx/signals';
-import { BooksService } from '../books/books.service';
+import { BooksDataService } from '../books/data.service';
 import { withBasket } from './with-basket.feature';
 import { withBooks } from './with-books.feature';
 import { withSearchTerm } from './with-search-term';
-
-// export interface AppState {
-//   book: Book | null;
-//   basket: Book[];
-// }
-
-// const initialState: AppState = {
-//   book: null,
-//   basket: [],
-// };
 
 export const AppStore = signalStore(
   { providedIn: 'root' },
   withDevtools('books'),
   withBasket(),
-  withBooks(BooksService),
+  withBooks(BooksDataService),
   withSearchTerm(),
   withHooks({
     onInit(store) {

70b2fb969cb2ccbf96ab07ac8089e6bb9011d4b8
dvir.monajem <dmenajem@gmail.com>
2024-03-17 17:26:46 +0200

    places - remove place store

--- apps/travel-on/src/app/places/places.service.ts
+++ apps/travel-on/src/app/places/places.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import {
-  Firestore,
   CollectionReference,
+  Firestore,
   collection,
   getDocs,
-  query,
-  limit,
+  query
 } from '@angular/fire/firestore';
+import { EntityResult } from '@dom';
 import { Observable, from, map } from 'rxjs';
 import { Places } from './places.model';
-import { EntityResult } from '@dom';
 
 @Injectable({
   providedIn: 'root',
@@ -23,17 +22,6 @@ export class PlacesService {
       this.firestore,
       this.VACATIONS_COLLECTION
     ) as CollectionReference<Places>;
-  }
-
-  getPlaces$(): Observable<Places[]> {
-    const queryRef = query(this.vacationsRef);
-    return from(getDocs(queryRef)).pipe(
-      map((querySnapshot) =>
-        querySnapshot.docs.map((doc) => {
-          return { ...doc.data(), id: doc.id } as Places;
-        })
-      )
-    );
   }
 
   loadPlaces(): Observable<EntityResult<Places>> {

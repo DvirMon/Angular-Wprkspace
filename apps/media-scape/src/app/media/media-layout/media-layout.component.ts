3138d8f56e64ccd9645b62c5fe9c018a7d833fd5
dvir.monajem <dmenajem@gmail.com>
2024-03-28 19:13:40 +0200

    sort and filter works on media-layout

--- apps/media-scape/src/app/media/media-layout/media-layout.component.ts
+++ apps/media-scape/src/app/media/media-layout/media-layout.component.ts
@@ -2,20 +2,35 @@ import {
   ChangeDetectionStrategy,
   Component,
   OnInit,
+  Signal,
   inject,
 } from '@angular/core';
 import { AppStore } from '../../store/store';
+import { Result } from '../../shared/types';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'ms-media-layout',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './media-layout.component.html',
   styleUrl: './media-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MediaLayoutComponent implements OnInit {
   #store = inject(AppStore);
+
+  media: Signal<Result[]>;
+  movies: Signal<Result[]>;
+  series: Signal<Result[]>;
+  games: Signal<Result[]>;
+
+  constructor() {
+    this.media = this.#store.entities;
+    this.movies = this.#store.movies;
+    this.series = this.#store.series;
+    this.games = this.#store.games;
+  }
 
   ngOnInit(): void {
     this.#store.loadMedia();

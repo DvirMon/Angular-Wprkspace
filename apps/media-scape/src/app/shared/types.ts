6604049e0d5c2964083505a45cc2398b2fd3816b
dvir.monajem <dmenajem@gmail.com>
2024-03-28 19:08:55 +0200

    add computed sort

--- apps/media-scape/src/app/shared/types.ts
+++ apps/media-scape/src/app/shared/types.ts
@@ -16,3 +16,8 @@ export enum MediaType {
   SERIES = 'series',
   GAME = 'game',
 }
+
+export enum SortDir {
+  ASC = 'asc',
+  DESC = 'desc',
+}
--- apps/media-scape/src/app/store/store.ts
+++ apps/media-scape/src/app/store/store.ts
@@ -12,12 +12,17 @@ import { addEntities, withEntities } from '@ngrx/signals/entities';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { EMPTY, pipe, switchMap } from 'rxjs';
 import { MediaService } from '../media/media.service';
-import { MediaType, Result } from '../shared/types';
+import { MediaType, Result, SortDir } from '../shared/types';
+
+interface State {
+  totalResults: number;
+  sortDir: SortDir;
+}
 
 export const AppStore = signalStore(
   { providedIn: 'root' },
   withDevtools('store'),
-  withState({ totalResults: 0 }),
+  withState<State>({ totalResults: 0, sortDir: SortDir.ASC }),
   withEntities<Result>(),
   withMethods((store, service = inject(MediaService)) => ({
     loadMedia: rxMethod<void>(
@@ -44,17 +49,54 @@ export const AppStore = signalStore(
   })),
   withComputed((store) => ({
     movies: computed(() =>
-      store.entities().filter((item) => compareType(item, MediaType.MOVIE))
+      store
+        .entities()
+        .filter(compareType(MediaType.MOVIE))
+        .sort((a, b) => compareName(a, b, store.sortDir()))
     ),
     series: computed(() =>
-      store.entities().filter((item) => compareType(item, MediaType.SERIES))
+      store.entities().filter(compareType(MediaType.SERIES)).sort(compareName)
     ),
     games: computed(() =>
-      store.entities().filter((item) => compareType(item, MediaType.GAME))
+      store.entities().filter(compareType(MediaType.GAME)).sort(compareName)
     ),
   }))
 );
 
-function compareType(item: Result, value: MediaType) {
-  return item.Type === value;
+// function compareType(item: Result, value: MediaType) {
+//   return item.Type === value;
+// }
+
+function compareType(value: MediaType) {
+  return function (item: Result): boolean {
+    return item.Type === value;
+  };
+}
+
+function compareName(
+  item1: Result,
+  item2: Result,
+  mode: SortDir = SortDir.ASC
+): number {
+  const title1 = item1.Title.toUpperCase();
+  const title2 = item2.Title.toUpperCase();
+
+  if (mode === SortDir.ASC) {
+    if (title1 < title2) {
+      return -1;
+    }
+    if (title1 > title2) {
+      return 1;
+    }
+    return 0;
+  } else {
+    // descending order
+    if (title1 > title2) {
+      return -1;
+    }
+    if (title1 < title2) {
+      return 1;
+    }
+    return 0;
+  }
 }

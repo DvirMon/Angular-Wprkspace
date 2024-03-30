3c48c678908e3f0cdaed3e65644ccf6921a70a5f
dvir.monajem <dmenajem@gmail.com>
2024-03-30 20:16:20 +0300

    add media-page-info-card

--- apps/media-scape/src/app/pages/item/item-page.component.ts
+++ apps/media-scape/src/app/pages/item/item-page.component.ts
@@ -1,89 +1,23 @@
 import {
-  KeyValuePipe,
-  NgFor,
-  NgOptimizedImage,
-  TitleCasePipe,
-} from '@angular/common';
-import {
   ChangeDetectionStrategy,
   Component,
-  Injector,
-  Signal,
-  WritableSignal,
-  computed,
-  effect,
-  inject,
-  input,
-  runInInjectionContext,
-  signal,
+  input
 } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import {
-  MatCard,
-  MatCardActions,
-  MatCardContent,
-  MatCardImage,
-} from '@angular/material/card';
-import { FormatDatePipe } from '../../shared/pipes/formatDate.pipe';
-import { MediaResult } from '../../shared/types';
-import { AppStore } from '../../store/store';
-import { Router } from '@angular/router';
+import { MediaInfoCardComponent } from '../../media/media-info-card/media-info-card.component';
 
 @Component({
   selector: 'ms-item-page',
   standalone: true,
   imports: [
-    NgOptimizedImage,
-    NgFor,
-    KeyValuePipe,
-    TitleCasePipe,
-    FormatDatePipe,
-    MatCard,
-    MatCardImage,
-    MatCardContent,
-    MatCardActions,
-    MatButton,
+    MediaInfoCardComponent
   ],
   templateUrl: './item-page.component.html',
   styleUrl: './item-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ItemPageComponent {
-  #injector = inject(Injector);
-  #store = inject(AppStore);
 
   imdbID = input.required<string>();
 
-  media: WritableSignal<MediaResult> = signal({} as MediaResult);
-
-  content: Signal<Partial<MediaResult>>;
-
-  showImg = signal(true);
-
-  constructor() {
-    effect(
-      () => {
-        this.media.update(() => ({
-          ...this.#store.entityMap()[this.imdbID()],
-        }));
-      },
-      { allowSignalWrites: true }
-    );
-
-    this.content = computed(() => ({
-      Title: this.media().Title,
-      Year: this.media().Year,
-      Type: this.media().Type,
-    }));
-  }
-
-  onError() {
-    this.showImg.update((value) => !value);
-  }
-
-  onReturn() {
-    runInInjectionContext(this.#injector, () =>
-      inject(Router).navigateByUrl('/')
-    );
-  }
+ 
 }

4094f80bf701541b3d3321c88ed6e9892227d019
dvir.monajem <dmenajem@gmail.com>
2024-03-30 12:00:06 +0300

    add button toggle

--- apps/media-scape/src/app/media/media-menu/media-menu.component.ts
+++ apps/media-scape/src/app/media/media-menu/media-menu.component.ts
@@ -1,19 +1,27 @@
+import { TitleCasePipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   Signal,
   inject,
 } from '@angular/core';
+import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
 import { MatListItem, MatNavList } from '@angular/material/list';
-import { AppStore } from '../../store/store';
-import { TitleCasePipe } from '@angular/common';
-import { MediaType } from '../../shared/types';
 import { PluralizePipe } from '../../shared/pipes/pluralize.pipe copy';
+import { MediaType } from '../../shared/types';
+import { AppStore } from '../../store/store';
 
 @Component({
   selector: 'ms-media-menu',
   standalone: true,
-  imports: [TitleCasePipe, PluralizePipe, MatNavList, MatListItem],
+  imports: [
+    TitleCasePipe,
+    PluralizePipe,
+    MatNavList,
+    MatListItem,
+    MatButtonToggleGroup,
+    MatButtonToggle,
+  ],
   templateUrl: './media-menu.component.html',
   styleUrl: './media-menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,

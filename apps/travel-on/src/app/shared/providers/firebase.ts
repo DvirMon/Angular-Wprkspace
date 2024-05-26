61b97bd096dbc27f97b322fb8b1e0ebb788c08aa
dvir.monajem <dmenajem@gmail.com>
2024-05-26 20:18:58 +0300

    travel-on - update dom-input props

--- apps/travel-on/src/app/shared/providers/firebase.ts
+++ apps/travel-on/src/app/shared/providers/firebase.ts
@@ -1,4 +1,3 @@
-import { importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -6,10 +5,8 @@ import { environment } from '../../../environments/environment';
 
 export function provideFirebase() {
   return [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-      provideAuth(() => getAuth()),
-      provideFirestore(() => getFirestore()),
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ];
 }

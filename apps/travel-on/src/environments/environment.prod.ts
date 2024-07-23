9a666703bb67fed2a6033b9b7745b3eaf4909490
dvir.monajem <dmenajem@gmail.com>
2024-07-23 18:03:00 +0300

    update ngrx/signals

--- apps/travel-on/src/environments/environment.prod.ts
+++ apps/travel-on/src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
-  server: "http://localhost:3000",
+  server: 'http://localhost:3000',
   firebaseConfig: {
-    apiKey: "AIzaSyAWE61Vm0CpfUtHq4G48aJVMbdY6REEtrA",
-    authDomain: "https://ng-travel-on.firebaseapp.com",
+    apiKey: 'AIzaSyAWE61Vm0CpfUtHq4G48aJVMbdY6REEtrA',
+    authDomain: 'travel-on-811d5.firebaseapp.com',
     databaseURL:
-      "https://travel-on-811d5-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "travel-on-811d5",
-    storageBucket: "travel-on-811d5.appspot.com",
-    messagingSenderId: "443306265288",
-    appId: "1:443306265288:web:0362cd9bd5a605e5d533fd",
-    measurementId: "G-BSDTG44DV2",
+      'https://travel-on-811d5-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'travel-on-811d5',
+    storageBucket: 'travel-on-811d5.appspot.com',
+    messagingSenderId: '443306265288',
+    appId: '1:443306265288:web:0362cd9bd5a605e5d533fd',
+    measurementId: 'G-BSDTG44DV2',
   },
-  email: "",
+  email: '',
 };

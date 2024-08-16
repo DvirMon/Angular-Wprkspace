12101a21db46adb5c8e2649d5536904992c2955d
dvir.monajem <dmenajem@gmail.com>
2024-08-16 12:27:45 +0300

    ✨ feat:  ErrorService
    add ErrorService handler

---
+++ apps/travel-on/src/app/shared/error.service.ts
@@ -0,0 +1,24 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorHandler, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorsService implements ErrorHandler {
+  handleError(error: ErrorEvent | HttpErrorResponse) {
+
+
+    console.log(error)
+
+    if (error instanceof ErrorEvent) {
+      this.handleClientError(error);
+    }
+  }
+
+  private handleClientError(error: ErrorEvent): void {
+    console.error(
+      'Custom Error Handler:',
+      error.message || 'An unexpected error occurred'
+    );
+  }
+}

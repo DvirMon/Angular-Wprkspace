106b40495eb229dd30979df0651d6f74f6a15388
dvir.monajem <dmenajem@gmail.com>
2024-08-16 09:52:24 +0300

    remove un use AuthService

--- apps/travel-on/src/app/auth/auth-forms/reset/index.ts
+++ apps/travel-on/src/app/auth/auth-forms/reset/index.ts
@@ -1,3 +1,2 @@
 export { ResetContactFormComponent } from "./reset-contact-form/reset-contact-form.component";
 export { ResetPasswordFormComponent } from "./reset-password-form/reset-password-form.component";
-export { ResetService } from "./reset.service";

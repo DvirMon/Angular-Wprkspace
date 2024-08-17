fe9fececd8eb81fb9b5405c79dfec6e958351060
dvir.monajem <dmenajem@gmail.com>
2024-08-17 19:08:50 +0300

    🦄 refactor(login.component): Hide AuthStore
    use token to hide AuthStore interface

--- apps/travel-on/src/app/auth/store/store.ts
+++ apps/travel-on/src/app/auth/store/store.ts
@@ -1,14 +1,12 @@
 // import { withDevtools } from '@angular-architects/ngrx-toolkit';
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
-import {
-  signalStore,
-  withState
-} from '@ngrx/signals';
+import { signalStore, withState } from '@ngrx/signals';
 import { initialState } from './auth.state';
 import { withAuthComputed } from './with-auth-computed';
 import { withAuthMethods } from './with-auth-methods';
 
 export const AuthStore = signalStore(
+  { providedIn: 'root' },
   withDevtools('auth'),
   withState(initialState),
   withAuthMethods(),
--- apps/travel-on/src/app/pages/login/login.component.ts
+++ apps/travel-on/src/app/pages/login/login.component.ts
@@ -1,18 +1,36 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Injector,
-  Signal,
-  WritableSignal,
   effect,
   inject,
+  InjectionToken,
+  Injector,
+  Provider,
   runInInjectionContext,
+  Signal,
   signal,
+  WritableSignal,
 } from '@angular/core';
 import { Router } from '@angular/router';
 import { CardButtonComponent, FormServerError } from '@dom/components';
+import { Observable, Unsubscribable } from 'rxjs';
 import { LoginFormComponent, SignInEvent } from '../../auth';
 import { AuthStore } from '../../auth/store/store';
+
+interface LoginStore {
+  isLoaded: Signal<boolean>;
+  loginError: Signal<FormServerError | undefined>;
+  signIn: (
+    input: SignInEvent | Observable<SignInEvent> | Signal<SignInEvent>
+  ) => Unsubscribable;
+  login: () => void;
+}
+
+const LOGIN_STORE = new InjectionToken<LoginStore>('LOGIN_STORE');
+
+function provideLoginStore(): Provider {
+  return { provide: LOGIN_STORE, useExisting: AuthStore };
+}
 
 @Component({
   selector: 'to-login-page',
@@ -21,10 +39,11 @@ import { AuthStore } from '../../auth/store/store';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideLoginStore()],
 })
 export class LoginPageComponent {
   #injector = inject(Injector);
-  #authStore = inject(AuthStore);
+  #authStore = inject(LOGIN_STORE);
 
   public readonly showOtp: WritableSignal<boolean>;
   public readonly serverError: Signal<FormServerError | undefined>;

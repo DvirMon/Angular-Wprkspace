70038a0b4f668c901d46356dfed3cc1ab8ff588f
dvir.monajem <dmenajem@gmail.com>
2024-10-06 17:23:08 +0300

    update tests

--- libs-utils/src/lib/ng-filters-lib/tests/evaluate-filter.service.spec.ts
+++ libs-utils/src/lib/ng-filters-lib/tests/evaluate-filter.service.spec.ts
@@ -1,6 +1,10 @@
 import { EvaluateFilterService } from '../evaluate-filter.service';
 import { FilterStrategyService } from '../filter-strategy.service';
-import { FilterCriteria, LOGICAL_OPERATOR } from '../filter.types';
+import {
+  EvaluateConfig,
+  FilterCriteria,
+  LOGICAL_OPERATOR,
+} from '../filter.types';
 import { TestBed } from '@angular/core/testing';
 
 // Fake for FilterStrategyService
@@ -39,46 +43,47 @@ describe('EvaluateFilterService', () => {
   });
 
   it('should filter an array of numbers with "greaterThan" criteria', () => {
-    const criteria: FilterCriteria[] = [
-      { key: 'value', operation: 'greaterThan', value: 10 },
-    ];
+    const config: EvaluateConfig = {
+      criteria: [{ key: 'value', operation: 'greaterThan', value: 10 }],
+    };
 
     const items = [{ value: 5 }, { value: 15 }, { value: 20 }];
 
-    const filtered = items.filter(service.evaluate(criteria));
+    const filtered = items.filter(service.evaluate(config));
 
     expect(filtered).toEqual([{ value: 15 }, { value: 20 }]);
   });
 
   it('should filter an array of strings with "equals" criteria', () => {
-    const criteria: FilterCriteria[] = [
-      { key: 'name', operation: 'equals', value: 'John' },
-    ];
-
+    const config: EvaluateConfig = {
+      criteria: [{ key: 'name', operation: 'equals', value: 'John' }],
+    };
     const items = [{ name: 'John' }, { name: 'Jane' }, { name: 'John' }];
 
-    const filtered = items.filter(service.evaluate(criteria));
+    const filtered = items.filter(service.evaluate(config));
 
     expect(filtered).toEqual([{ name: 'John' }, { name: 'John' }]);
   });
 
   it('should return an empty array when no items match "equals" criteria', () => {
-    const criteria: FilterCriteria[] = [
+    const config = { criteria:  [
       { key: 'name', operation: 'equals', value: 'Michael' },
-    ];
+    ]};
 
     const items = [{ name: 'John' }, { name: 'Jane' }, { name: 'Jake' }];
 
-    const filtered = items.filter(service.evaluate(criteria));
+    const filtered = items.filter(service.evaluate(config));
 
     expect(filtered).toEqual([]);
   });
 
   it('should filter an array of numbers with "AND" logic for multiple criteria', () => {
-    const criteria: FilterCriteria[] = [
-      { key: 'age', operation: 'greaterThan', value: 18 },
-      { key: 'score', operation: 'greaterThan', value: 80 },
-    ];
+    const config = {
+      criteria: [
+        { key: 'age', operation: 'greaterThan', value: 18 },
+        { key: 'score', operation: 'greaterThan', value: 80 },
+      ]
+    }; 
 
     const items = [
       { age: 20, score: 90 },
@@ -86,7 +91,7 @@ describe('EvaluateFilterService', () => {
       { age: 22, score: 75 },
     ];
 
-    const filtered = items.filter(service.evaluate(criteria));
+    const filtered = items.filter(service.evaluate(config));
 
     expect(filtered).toEqual([{ age: 20, score: 90 }]);
   });
@@ -113,10 +118,12 @@ describe('EvaluateFilterService', () => {
     });
 
     it('should filter an array with "OR" logic when LOGICAL_OPERATOR is set to "OR"', () => {
-      const criteria: FilterCriteria[] = [
-        { key: 'age', operation: 'greaterThan', value: 18 },
-        { key: 'score', operation: 'greaterThan', value: 80 },
-      ];
+      const config = {
+        criteria: [
+          { key: 'age', operation: 'greaterThan', value: 18 },
+          { key: 'score', operation: 'greaterThan', value: 80 },
+        ]
+      };
 
       const items = [
         { age: 20, score: 70 },
@@ -124,7 +131,7 @@ describe('EvaluateFilterService', () => {
         { age: 15, score: 75 },
       ];
 
-      const filtered = items.filter(service.evaluate(criteria));
+      const filtered = items.filter(service.evaluate(config));
 
       expect(filtered).toEqual([
         { age: 20, score: 70 },
@@ -134,11 +141,11 @@ describe('EvaluateFilterService', () => {
   });
 
   it('should return the original array when no criteria is provided', () => {
-    const criteria: FilterCriteria[] = [];
+    const config  = {criteria: []} 
 
     const items = [{ value: 5 }, { value: 15 }, { value: 20 }];
 
-    const filtered = items.filter(service.evaluate(criteria));
+    const filtered = items.filter(service.evaluate(config));
 
     expect(filtered).toEqual(items);
   });

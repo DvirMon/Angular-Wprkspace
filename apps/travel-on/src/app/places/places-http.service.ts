64d38d1cc5f0c1c7a973cc33acef7d89c7faf25f
dvir.monajem <dmenajem@gmail.com>
2024-10-10 12:08:35 +0300

    return template + remove tap

--- apps/travel-on/src/app/places/places-http.service.ts
+++ apps/travel-on/src/app/places/places-http.service.ts
@@ -1,20 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { Places } from './places.model';
+import { Observable } from 'rxjs';
 import { API_URL } from '../shared/tokans';
+import { Places } from './places.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesHttpService {
-
   readonly #http = inject(HttpClient);
   readonly #apiUrl = inject(API_URL);
 
   public loadPlaces(): Observable<Places[]> {
     const url = `${this.#apiUrl}/places`;
-    return this.#http.get<Places[]>(url).pipe(tap((v) => console.log(v)));
+    return this.#http.get<Places[]>(url);
   }
-
 }

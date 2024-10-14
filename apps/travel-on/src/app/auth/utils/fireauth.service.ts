8e63a697c8f775bcb2d7a75ac8a651c4d7ec94d1
dvir.monajem <dmenajem@gmail.com>
2024-10-14 19:00:00 +0300

    🦄 refactor(firebase.service): Refactor with defer
    use differ operator instead of from + return firebase/auth as obs

--- apps/travel-on/src/app/auth/utils/fireauth.service.ts
+++ apps/travel-on/src/app/auth/utils/fireauth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, Injector, inject } from '@angular/core';
 import {
-  ActionCodeSettings,
-  ConfirmationResult,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
   UserCredential,
 } from '@angular/fire/auth';
-import { Observable, from, map } from 'rxjs';
-import { generateVerificationLink } from './auth.helpers';
+import { Observable, defer, from, switchMap } from 'rxjs';
 
 export interface FirebaseError {
   code: string;
@@ -16,105 +16,33 @@ export interface FirebaseError {
 
 @Injectable({ providedIn: 'root' })
 export class FireAuthService {
-  private readonly injector = inject(Injector);
-  // private readonly auth = inject(Auth);
-
-  private loadFirebaseAuth() {
-    return import('firebase/auth').then((firebase) => {
-      return firebase;
-    });
+  #loadFirebaseAuth() {
+    return from(
+      import('firebase/auth').then((firebase) => {
+        return firebase;
+      })
+    );
   }
-
   public createInWithEmailAndPassword$(
     email: string,
     password: string
   ): Observable<UserCredential> {
-    return from(
-      this.loadFirebaseAuth().then(
-        ({ getAuth, createUserWithEmailAndPassword }) => {
-          const auth = getAuth();
-          return createUserWithEmailAndPassword(auth, email, password);
-        }
-      )
-    );
-  }
-
-  // Sign in with phone number and reCAPTCHA verification.
-  public signInWithPhone$(phone: string): Observable<ConfirmationResult> {
-    return from(
-      this.loadFirebaseAuth().then(
-        ({ getAuth, signInWithPhoneNumber, RecaptchaVerifier }) => {
-          const auth = getAuth();
-          const recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha', {
-            size: 'invisible',
-          });
-          return signInWithPhoneNumber(auth, phone, recaptchaVerifier);
-        }
-      )
-    );
-  }
-
-  // Send a sign-in link (magic link) to the provided email.
-  public sendSignInLinkToEmail$(email: string): Observable<string> {
-    const actionCodeSettings: ActionCodeSettings = {
-      url: generateVerificationLink(this.injector),
-      handleCodeInApp: true,
-    };
-
-    return from(
-      this.loadFirebaseAuth().then((firebase) => {
-        const auth = firebase.getAuth();
-        return firebase.sendSignInLinkToEmail(auth, email, actionCodeSettings);
+    return this.#loadFirebaseAuth().pipe(
+      switchMap(({ getAuth, createUserWithEmailAndPassword }) => {
+        const auth = getAuth();
+        // Use 'from' to convert the promise to an observable
+        return defer(() =>
+          createUserWithEmailAndPassword(auth, email, password)
+        );
       })
-    ).pipe(map(() => email));
+    );
   }
-
   // Check if the provided email link is a valid sign-in link.
-  public isSignInWithEmailLink$(emailLink: string): Observable<boolean> {
-    return from(
-      this.loadFirebaseAuth().then((firebase) => {
-        const auth = firebase.getAuth();
-        return firebase.isSignInWithEmailLink(auth, emailLink);
-      })
-    );
-  }
-
-  // // Private method: Sign in with Google OAuth provider.
-  // public signInWithGoogle$(): Observable<UserCredential> {
-  //   return from(signInWithPopup(this.auth, new GoogleAuthProvider()));
-  // }
-
   public signInWithGoogle$(): Observable<UserCredential> {
-    return from(
-      this.loadFirebaseAuth().then(
-        ({ getAuth, signInWithPopup, GoogleAuthProvider }) => {
-          const auth = getAuth();
-          return signInWithPopup(auth, new GoogleAuthProvider());
-        }
-      )
-      // (async () => {
-      //   // Dynamically import Firebase and required modules
-      //   const { getAuth, signInWithPopup, GoogleAuthProvider } = await import(
-      //     'firebase/auth'
-      //   );
-
-      //   // Initialize Firebase Auth
-      //   const auth = getAuth();
-
-      //   // Sign in with Google
-      // })()
-    );
-  }
-
-  // Sign in with email link (magic link).
-  public signInWithEmailLink$(
-    email: string,
-    emailLink: string
-  ): Observable<UserCredential> {
-    return from(
-      this.loadFirebaseAuth().then((firebase) => {
-        const auth = firebase.getAuth();
-        return firebase.signInWithEmailLink(auth, email, emailLink);
+    return this.#loadFirebaseAuth().pipe(
+      switchMap(({ getAuth, signInWithPopup, GoogleAuthProvider }) => {
+        const auth = getAuth();
+        return from(signInWithPopup(auth, new GoogleAuthProvider()));
       })
     );
   }
@@ -124,20 +52,25 @@ export class FireAuthService {
     email: string,
     password: string
   ): Observable<UserCredential> {
-    return from(
-      this.loadFirebaseAuth().then((firebase) => {
-        const auth = firebase.getAuth();
-        return firebase.signInWithEmailAndPassword(auth, email, password);
+    return this.#loadFirebaseAuth().pipe(
+      switchMap(({ getAuth, signInWithEmailAndPassword }) => {
+        const auth = getAuth();
+        const userCredential = signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        return defer(() => userCredential);
       })
     );
   }
 
   // Send password reset email.
   public sendPasswordResetEmail(email: string): Observable<void> {
-    return from(
-      this.loadFirebaseAuth().then((firebase) => {
+    return this.#loadFirebaseAuth().pipe(
+      switchMap((firebase) => {
         const auth = firebase.getAuth();
-        return firebase.sendPasswordResetEmail(auth, email);
+        return defer(() => firebase.sendPasswordResetEmail(auth, email));
       })
     );
   }
@@ -147,10 +80,12 @@ export class FireAuthService {
     oobCode: string,
     newPassword: string
   ): Observable<void> {
-    return from(
-      this.loadFirebaseAuth().then((firebase) => {
+    return this.#loadFirebaseAuth().pipe(
+      switchMap((firebase) => {
         const auth = firebase.getAuth();
-        return firebase.confirmPasswordReset(auth, oobCode, newPassword);
+        return defer(() =>
+          firebase.confirmPasswordReset(auth, oobCode, newPassword)
+        );
       })
     );
   }

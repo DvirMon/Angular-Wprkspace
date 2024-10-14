12dfc61d09512d4050b9156059ada8189205d48a
dvir.monajem <dmenajem@gmail.com>
2024-10-14 19:33:08 +0300

    replace firebase with http logic

--- apps/travel-on/src/app/pages/register/register.service.ts
+++ apps/travel-on/src/app/pages/register/register.service.ts
@@ -2,6 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { FireAuthService, Register } from '../../auth';
 import { Observable } from 'rxjs';
 import { UserCredential } from '@angular/fire/auth';
+import { HttpClient } from '@angular/common/http';
+import { API_URL } from '../../shared/tokens';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,12 @@ import { UserCredential } from '@angular/fire/auth';
 export class RegisterService {
   readonly #fireAuthService = inject(FireAuthService);
 
+  readonly #http = inject(HttpClient);
+  readonly #apiUrl = inject(API_URL);
+
+  readonly #url = this.#apiUrl + '/auth/register';
+
   public register$({ password, email }: Register): Observable<UserCredential> {
-    return this.#fireAuthService.createInWithEmailAndPassword$(email, password);
+    return this.#http.post<UserCredential>(this.#url, { email, password });
   }
 }
